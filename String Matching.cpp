f01bdaa7afc2e5406e6f6db008616ff8433ae0e7
akshays1ngh <96130904+akshays1ngh@users.noreply.github.com>
2023-02-25 09:53:23 +0530

    Add files via upload

---
+++ String Matching.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	
+	string s,patt;
+	cout<<"Enter string: "<<endl;
+	getline(cin,s);
+	cout<<"Enter pattern to be found: "<<endl;
+	getline(cin,patt);
+	
+	int n=s.length(),m=patt.length(),i,j;
+	
+	for(i=0;i<=n-m;i++){
+	    
+		for(j=0;j<m;j++){
+			if(s[i+j]!=patt[j])
+				break;
+		}
+		if(j==m){
+			cout<<"Pattern found at index: "<<i<<endl;
+		    break;
+		}
+	}
+	return 142;
+}	
+	
