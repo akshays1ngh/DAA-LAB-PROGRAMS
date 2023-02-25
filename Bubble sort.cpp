ad6969aec92625ad73d67ca48ec355f245ed5c52
akshays1ngh <96130904+akshays1ngh@users.noreply.github.com>
2023-02-25 09:48:30 +0530

    Add files via upload

---
+++ Bubble sort.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int n,t;
+	cout<<"Enter the value of n: "<<endl;
+	cin>>n;
+	int arr[n];
+	
+	int i,j;
+	cout<<"Enter elements: "<<endl;
+	for(i=0;i<n;i++)
+		cin>>arr[i];
+	
+	for(i=0;i<n-1;i++){
+		for(j=0;j<n-i-1;j++){
+			if(arr[j]>arr[j+1]){
+				t=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=t;
+			}
+		}
+	}
+	
+	cout<<"Ordered elements: "<<endl;
+	
+	for(i=0;i<n;i++)
+		cout<<arr[i]<<" ";
+	return 142;
+}
