338140f6a146e63351a0cff3c4d33d9bd909afd3
akshays1ngh <96130904+akshays1ngh@users.noreply.github.com>
2023-02-25 09:51:54 +0530

    Add files via upload

---
+++ Selection Sort.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int n,t;
+	cout<<"Enter the value of n: "<<endl;
+	cin>>n;
+	int arr[n];
+	
+	int i,j;
+	cout<<"Enter elements: "<<endl;
+	for(i=0;i<n;i++)
+		cin>>arr[i];
+	int min;
+	for(i=0;i<n-1;i++){
+		min=i;
+		for(j=i+1;j<n;j++){
+			if(arr[j]<arr[min])
+				min=j;
+		}
+		t=arr[i];
+		arr[i]=arr[min];
+		arr[min]=t;
+	}
+	
+	cout<<"Ordered elements: "<<endl;
+	
+	for(i=0;i<n;i++)
+		cout<<arr[i]<<" ";
+	return 142;
+}
