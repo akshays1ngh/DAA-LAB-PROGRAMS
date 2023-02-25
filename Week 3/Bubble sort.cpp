357564f5b6bbd5917ca73e8b4983f8727f9432db
akshays1ngh <96130904+akshays1ngh@users.noreply.github.com>
2023-02-25 10:06:36 +0530

    Update Bubble sort.cpp

--- Week 3/Bubble sort.cpp
+++ Week 3/Bubble sort.cpp
@@ -14,14 +14,14 @@ int main(){
 		cin>>arr[i];
 	
 	for(i=0;i<n-1;i++){
-		for(j=0;j<n-i-1;j++){
-			if(arr[j]>arr[j+1]){
-				t=arr[j];
-				arr[j]=arr[j+1];
-				arr[j+1]=t;
-			}
+	  for(j=0;j<n-i-1;j++){
+		if(arr[j]>arr[j+1]){
+		   t=arr[j];
+		   arr[j]=arr[j+1];
+		   arr[j+1]=t;
 		}
 	}
+}
 	
 	cout<<"Ordered elements: "<<endl;
 	
